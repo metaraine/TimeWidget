32c08b62422c0e3e04eb9f3b7f890ac5888d23b8
David Handel <handeld@gmail.com>
2013-06-14 19:01:53 -0400

    loading the clock cause the page to be blank

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 $(function() {
-
-
-
-
-	$( ":submit" ).click(function() {
+	// $( ":submit" ).click(function() {
+      $(".timeIn").on("keyup", function() {
 		var 	input =  $(".timeIn").val(),	
-		  	dateObject = new Date(input),
+		  	dateObject = moment(input),
 		  	dateObjToStr = dateObject.toString();
-	
+                  window.globalHour = dateObject.hour();
+                  window.globalMinutes = dateObject.minutes();
+                  
+                  // globals to be passed outside the ready function and read below into the clock
+                  // the Google chart api can't be accessed inside the ready function because it yields a blnk page
+                  window.globalHour =dateObject.hour();
+                  window.globalMinute =dateObject.minute();
+
+      if (!dateObject.isValid()) {
+        $(".EST").text("Not a valid time/date.");
+        $(".CST").text("Not a valid time/date.");   
+        $(".MST").text("Not a valid time/date.");   
+        $(".PST").text("Not a valid time/date.");            
+      }
+        else {
 		$(".EST").text(moment(dateObjToStr).format("h:mm A, dddd, MMMM Do, YYYY"));
 		$(".CST").text(moment(dateObjToStr).subtract("h", 1).format("h:mm A, dddd, MMMM Do, YYYY"));
 		$(".MST").text(moment(dateObjToStr).subtract("h", 2).format("h:mm A, dddd, MMMM Do, YYYY"));
 		$(".PST").text(moment(dateObjToStr).subtract("h", 3).format("h:mm A, dddd, MMMM Do, YYYY"));
+      }
 	})
 })
 
+$(".submit").on("click", function() {
+        var       input =  $(".timeIn").val(),  
+                  dateObject = moment(input);
+        // if (dateObject.isValid()) {
+          window.open
+          clock(window.globalHour,window.globalMinutes);
+      // }
+})
+
+function clock(hour, minutes) {
+   if(typeof(hour)==='undefined') a = 6;
+   if(typeof(minutes)==='undefined') b = 15;
       // Load the Visualization API and the piechart package.
       google.load('visualization', '1.0', {'packages':['corechart']});
 
@@ -31,10 +55,10 @@ $(function() {
         data.addColumn('string', 'Topping');
         data.addColumn('number', 'Slices');
         data.addRows([
-          ['hour', 1],
-          ['all else', 34],
-          ['minutes', 1],
-          ['all else', 26]
+          ['hour', hour],
+  
+          ['minutes', minutes],
+
         ]);
 
         // Set chart options
@@ -48,3 +72,4 @@ $(function() {
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
+    }
